a00739132c7244cee02ec42f244d4e23353f7c6c
liuze <aqliuze@163.com>
2018-07-01 16:34:28 +0800

    初始化

---
+++ XXTreeSouce/XXTreeSouce/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXTreeSouce
+//
+//  Created by 刘泽 on 2018/7/1.
+//  Copyright © 2018年 liuze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXTreeSouce/XXTreeSouce/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXTreeSouce
+//
+//  Created by 刘泽 on 2018/7/1.
+//  Copyright © 2018年 liuze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
